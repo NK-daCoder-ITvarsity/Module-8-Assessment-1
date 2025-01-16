ed9f72f83e080faa031bb9869192c3c04e5291d3
NK-daCoder <gpnkstudios@gmail.com>
2025-01-16 15:10:22 +0200

    updated routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,20 @@
 import React from 'react'
-import { BrowserRouter as Router, Outlet } from 'react-router-dom';
+import { HashRouter as Router, Outlet } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext'
 
 const App = () => {
   return (
     <>
-    <Router basename="/Module-8-Assessment-1">
-      <AuthProvider>
-        {/* Children */}
-        <main className="bg-gray-300 h-screen flex flex-col items-center justify-center">
-          <Outlet />
-        </main>
-      </AuthProvider>
-    </Router>
-      
+      <Router basename="/Module-8-Assessment-1">
+        <AuthProvider>
+          {/* Children */}
+          <main className="bg-gray-300 h-screen flex flex-col items-center justify-center">
+            <Outlet />
+          </main>
+        </AuthProvider>
+      </Router>
     </>
   )
 }
 
-export default App
+export default App;
