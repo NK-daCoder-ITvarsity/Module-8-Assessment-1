3a196b82b63977279bcbba72902db15410e1f9e5
NK-daCoder <gpnkstudios@gmail.com>
2025-01-16 15:21:39 +0200

    fixed routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,24 @@
-import React from 'react'
-import { HashRouter as Router, Outlet } from 'react-router-dom';
-import { AuthProvider } from './context/AuthContext'
+import React from 'react';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
+import Registration from './components/Registration';  // Import your Registration component
+import Home from './pages/Home';
 
 const App = () => {
   return (
     <>
       <Router basename="/Module-8-Assessment-1">
         <AuthProvider>
-          {/* Children */}
           <main className="bg-gray-300 h-screen flex flex-col items-center justify-center">
-            <Outlet />
+            <Routes>
+              <Route path="/" element={<Home />} />  {/* Add a Home route */}
+              <Route path="/registration" element={<Registration />} />  {/* Add the Registration route */}
+            </Routes>
           </main>
         </AuthProvider>
       </Router>
     </>
-  )
-}
+  );
+};
 
 export default App;
